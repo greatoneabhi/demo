1076621827e8949c39d9b5f7288ba8ac876abee3
Abhishek Singh <abhishek.dumca@gmail.com>
2020-12-12 23:07:21 +0530

    AES GSM

--- src/main/java/com/example/demo/ECIESImplementation.java
+++ src/main/java/com/example/demo/ECIESImplementation.java
@@ -4,6 +4,8 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 
 import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
 import java.math.BigInteger;
 import java.security.*;
 import java.security.interfaces.ECPrivateKey;
@@ -12,6 +14,10 @@ import java.security.spec.ECGenParameterSpec;
 
 public class ECIESImplementation {
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchProviderException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+        // Both Alice and Bob agree upon this value in some manner before starting this protocol.
+        byte[] iv = new SecureRandom().generateSeed(16);
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
         System.out.println("ECIES Implementation");
         Security.addProvider(new BouncyCastleProvider());
 
@@ -27,11 +33,11 @@ public class ECIESImplementation {
         ECPrivateKey privateKeyV = (ECPrivateKey) keyPairV.getPrivate();
         ECPublicKey publicKeyV = (ECPublicKey) keyPairV.getPublic();
 
-        System.out.println("Private key U: "+privateKeyU.getEncoded().toString());
+        System.out.println("Private key U: "+privateKeyU.toString());
         System.out.println("Public Key U: "+publicKeyU.toString());
 
-        System.out.println("Private key V: "+privateKeyU.getEncoded().toString());
-        System.out.println("Public Key V: "+publicKeyU.toString());
+        System.out.println("Private key V: "+privateKeyV.toString());
+        System.out.println("Public Key V: "+publicKeyV.toString());
 
         KeyAgreement ecdhU = KeyAgreement.getInstance("ECDH");
         ecdhU.init(privateKeyU);
@@ -41,12 +47,25 @@ public class ECIESImplementation {
         ecdhV.init(privateKeyV);
         ecdhV.doPhase(publicKeyU, true);
 
+        SecretKey secretKey = ecdhU.generateSecret("ECIES");
+
         System.out.println("Secret computed by U: 0x" + (new BigInteger(1, ecdhU.generateSecret())
                 .toString(16).toUpperCase()));
         System.out.println("Secret computed by V: 0x" + (new BigInteger(1, ecdhV.generateSecret())
                 .toString(16).toUpperCase()));
 
-        encryptionAndDecryption();
+        Cipher cipher = Cipher.getInstance("AES/GCM/NOPADDING", BouncyCastleProvider.PROVIDER_NAME);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new GCMParameterSpec(128, iv));
+        String message = "Hello World";
+
+        byte[] ciphertext = cipher.doFinal(message.getBytes());
+        System.out.println(Hex.toHexString(ciphertext));
+
+        Cipher decipher = Cipher.getInstance("AES/GCM/NOPADDING", BouncyCastleProvider.PROVIDER_NAME);
+        decipher.init(Cipher.DECRYPT_MODE, secretKey, new GCMParameterSpec(128, iv));
+        byte[] plaintext = decipher.doFinal(ciphertext);
+
+        System.out.println(new String(plaintext));
 
     }
 
@@ -59,7 +78,6 @@ public class ECIESImplementation {
         Cipher iesCipher = Cipher.getInstance("ECIES", BouncyCastleProvider.PROVIDER_NAME);
         Cipher iesDecipher = Cipher.getInstance("ECIES", BouncyCastleProvider.PROVIDER_NAME);
         iesCipher.init(Cipher.ENCRYPT_MODE, ecKeyPair.getPublic());
-
         String message = "Hello World";
 
         byte[] ciphertext = iesCipher.doFinal(message.getBytes());
